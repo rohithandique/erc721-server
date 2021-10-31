b3dcf653adbe9f4ee38061d3b14c1b0d813d5c01
Rohit Handique <rohit.handique.rh@gmail.com>
2021-10-31 20:26:51 +0530

    vercel edit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 module.exports = (request, response) => {
 
     const id = request.query.id;
-
+    
     response.json({"name": "Cryptopunks"+id,
     "description": "A highly acclaimed collection of punks.", 
     "image": "https://www.larvalabs.com/cryptopunks/cryptopunk"+id+".png"});
