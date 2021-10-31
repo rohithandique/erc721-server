10840ee459c17b4b14df79bfa9334681f5909ffa
Rohit Handique <rohit.handique.rh@gmail.com>
2021-10-31 20:20:35 +0530

    vercel edit

--- api/index.js
+++ api/index.js
@@ -1,17 +1,8 @@
-const app = require('express')();
+module.exports = (request, response) => {
 
-app.get('/api/:id', (req, res) => {
+    const id = request.query.id;
 
-    const id = req.params.id;
-    const response = 
-        {
-            "name": "Cryptopunks"+id,
-            "description": "A highly acclaimed collection of punks.", 
-            "image": "https://www.larvalabs.com/cryptopunks/cryptopunk"+id+".png"
-        }
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(response);
-});
-
-module.exports = app;
+    response.json({"name": "Cryptopunks"+id,
+    "description": "A highly acclaimed collection of punks.", 
+    "image": "https://www.larvalabs.com/cryptopunks/cryptopunk"+id+".png"});
+};
