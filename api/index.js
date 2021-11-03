cf1d3bd6b9089a493c0bb54be7cace9ea4fb0f52
Rohit Handique <rohit.handique.rh@gmail.com>
2021-11-04 02:33:37 +0530

    added cors header

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
-module.exports = (request, response) => {
+module.exports = (req, res) => {
 
-    const id = request.query.id;
-    
-    response.json({"name": "Cryptopunks"+id,
+    const id = req.query.id;
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    res.json({"name": "Cryptopunks"+id,
     "description": "A highly acclaimed collection of punks.", 
     "image": "https://www.larvalabs.com/cryptopunks/cryptopunk"+id+".png"});
 };
